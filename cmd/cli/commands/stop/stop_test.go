ea54f43c8708121015473bb6ef31b3e6223a84f8
Matty Evans <mattevansnz@gmail.com>
2025-01-10 13:32:26 +1000

    style: update error message in test files

--- cmd/cli/commands/stop/stop_test.go
+++ cmd/cli/commands/stop/stop_test.go
@@ -153,7 +153,7 @@ func TestRegisterCommands(t *testing.T) {
 		{
 			name:          "fails when config service fails",
 			configPath:    "/invalid/path/that/doesnt/exist",
-			expectedError: "error loading config",
+			expectedError: "directory [/invalid/path/that/doesnt/exist] does not exist",
 		},
 	}
 
